6e0b311b3299d5b1c5080d703cdab8c4c253ea3d
2300032867GandhamSwathiSree <2300032867@kluniversity.in>
2025-09-23 09:17:45 +0530

    Update ServletInitializer.java

--- src/main/java/art/ServletInitializer.java
+++ src/main/java/art/ServletInitializer.java
@@ -1,13 +1,8 @@
 package art;
 
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-public class ServletInitializer extends SpringBootServletInitializer {
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(ArtGalleryApplication.class);
-	}
+
+public class ServletInitializer {
 
 }
